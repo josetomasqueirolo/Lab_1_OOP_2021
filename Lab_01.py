efb55196dcb6fc8a222af0708425ce63f52006a7
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-03-23 13:30:31 -0300

    Commit 6

--- Lab_01.py
+++ Lab_01.py
@@ -17,25 +17,7 @@ def generarjuego(m):   # m=número de cartas
             tablero_real.append(tablero[i][j])
     return tablero_real
            
-# def generar_tablero(n):
-#     matriz = []
-#     for i in range(n):
-#         lista = [0]*n
-#         matriz.append(lista)
-#     return matriz
- 
-# def imprime_tablero(matriz):
-#     for fila in matriz:
-#         var = ""
-#         for elemento in fila:
-#             var+= str(elemento) + " "
-#         print(var)
-
-# n = int(input("tamaño tablero"))
-# imprime_matriz(generar_matriz(n)) 
-
-
-cc=int(input("Choose the number of cards: "))
+cc=int(input("Choose the number of cards (minimum 2): "))
 cartas_totales=cc*2
 tablero_respuestas=generarjuego(cc)
 print(tablero_respuestas)   #sacar print
@@ -55,7 +37,7 @@ print()
 
 while P1+P2<cc:
     # TURNO JUGADOR 1
-    print("~~~~Player1´s turn~~~~")
+    print("~~~~Player1's turn~~~~")
     coordenada1=int(input('Choose a coordinate: '))
     tablero_vacio[coordenada1]=tablero_respuestas[coordenada1]
     print(tablero_vacio)
@@ -75,7 +57,7 @@ while P1+P2<cc:
     print(tablero_vacio)
     print()
     
-    print("~~~~Player2´s turn~~~~")
+    print("~~~~Player2's turn~~~~")
     coordenada1=int(input('Choose a coordinate: '))
     tablero_vacio[coordenada1]=tablero_respuestas[coordenada1]
     print(tablero_vacio)
@@ -91,3 +73,10 @@ while P1+P2<cc:
         print("Nice!")
         P2+=1
 
+print()
+if P1>P2:
+    print("$$$$$Pleyer1 wins!$$$$$")
+elif P1<P2:
+    print("$$$$$Player2 wins!$$$$$")
+else:
+    print("It's a tie")
