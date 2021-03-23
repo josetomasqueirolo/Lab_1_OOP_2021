f932b76fbf30a81861d5f568c9b19eb1c3232535
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-03-23 12:01:00 -0300

    Commit 4

--- Lab_01.py
+++ Lab_01.py
@@ -17,31 +17,50 @@ def generarjuego(m):   # m=número de cartas
             tablero_real.append(tablero[i][j])
     return tablero_real
            
-        
-def generar_matriz(n):
-    matriz = []
-    for i in range(n):
-        lista = [0]*n
-        matriz.append(lista)
-    return matriz
+# def generar_tablero(n):
+#     matriz = []
+#     for i in range(n):
+#         lista = [0]*n
+#         matriz.append(lista)
+#     return matriz
  
-def imprime_matriz(matriz):
-    for fila in matriz:
-        var = ""
-        for elemento in fila:
-            var+= str(elemento) + " "
-        print(var)
+# def imprime_tablero(matriz):
+#     for fila in matriz:
+#         var = ""
+#         for elemento in fila:
+#             var+= str(elemento) + " "
+#         print(var)
 
-# n = int(input("tamaño matriz"))
+# n = int(input("tamaño tablero"))
 # imprime_matriz(generar_matriz(n)) 
 
 
 cc=int(input("Choose the number of cards: "))
-print(generarjuego(cc))
+cartas_totales=cc*2
+tablero_respuestas=generarjuego(cc)
+print(tablero_respuestas)   #sacar print
+print()
 
 P1=0
-P0=0
+P2=0
 
-print('Player 1 starts playing.')
+tablero_vacio=[]
+
+for i in range(cartas_totales):
+    tablero_vacio.append(0)
+    
+print("Player 1 starts playing.")
+print(tablero_vacio)
 
 
+while P1+P2<cc:
+    # TURNO JUGADOR 1
+    coordenada1_P1=int(input('Choose a coordinate: '))
+    
+    
+    coordenada2_P1=int(input('Choose a coordinate: '))
+    
+    print()
+    print(coordenada1_P1)
+    print(coordenada2_P1)   
+    break
